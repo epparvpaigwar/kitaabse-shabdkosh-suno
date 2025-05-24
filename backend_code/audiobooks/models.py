7ac27cb7fc0b8d95d1f1031d7dcd641a3f5c0dc6
Parvpaigwar <parvpaigwar@gmail.com>
2025-05-24 15:30:57 +0530

    done

--- backend_code/audiobooks/models.py
+++ backend_code/audiobooks/models.py
@@ -1,12 +1,11 @@
 from django.db import models
-# from users import 
-
+from users.models import User
 # Create your models here.
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100, blank=True)
-    # uploader = models.ForeignKey(User, on_delete=models.CASCADE)
+    uploader = models.ForeignKey(User, on_delete=models.CASCADE)
     pdf_file = models.FileField(upload_to='pdfs/')
     is_public = models.BooleanField(default=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
--- backend_code/backend/settings.py
+++ backend_code/backend/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     # Your app
     'audiobooks',
     'users'
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -141,3 +142,22 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+
+
+import os
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
